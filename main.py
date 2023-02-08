8a1cded693ceb7452f812aa869dd6a825dd1a476
William Denny <ispry3@gmail.com>
2023-02-07 23:36:22 -0500

    extracting odds

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ NBA_bdate = NBA_data['#Birth Date']
 #print(NBA_bdate)
 
 NBA_jers = NBA_jers.dropna()                    #drop null values if any
-array = (NBA_jers.to_numpy())                   #convert column to array
+array = (NBA_jers.to_numpy())                   #convert column to list
 size = len(array)
 
 selectionSort(array, size)                      #run selection sort TC: O(n^2) Space: O(1)
@@ -58,13 +58,27 @@ NFL_bdate = NFL_data['date_of_birth']
 NFL_team = NFL_data['team']
 #print(fname)
 
-#NBA_data[]
+"""
 for x in range(10):
     if x % 2 == 1:                              #choosing all the NFL players with odd jersey numbers between 0 and 10
-        print(NFL_data[NFL_data['number'].isin([x])])
+        print((NFL_data[NFL_data['number'].isin([x])]))
 
 for y in range(10, 21):
     if y % 2 == 0:                              #choosing all the NBA players with even jersey numbers between 10 and 20
         print(NBA_data[NBA_data['#Jersey Num'].isin([y])])
+"""
+evens_list = []                                             #creating empty sets for storage
+odds_list = []
+jers_list = NBA_jers.values.tolist()                        #converting dataframes to lists
+num_list = NFL_num.values.tolist()
+jers_list.sort()                                            #sorting lists
+num_list.sort()
 
-                                                #save both odd sets as its own data frame
+for x in range(1, 101, 2):
+    for y in range(len(jers_list)):                         #selecting odd numbers from list and storing them
+        if x == jers_list[y]:
+            odds_list.append(jers_list[y])
+
+#print(odds_list)
+odds_df = pd.DataFrame(odds_list, columns = ['odds'])       #converting list into dataframe
+print(odds_df)
